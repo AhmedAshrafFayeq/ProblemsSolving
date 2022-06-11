7694e35cc241417244c6ab161ec4b8e5a09bc892
Ahmed Fayeq <ahmedashraffayeq@gmail.com>
2022-06-11 10:37:26 +0200

    calculate seconds to (hours, mins, seconds)

--- Problems.playground/Contents.swift
+++ Problems.playground/Contents.swift
@@ -1,3 +1,14 @@
 import UIKit
 
-var greeting = "Hello, playground"
+// 1 : 70 : 70
+
+// 2 : 11 : 10
+
+
+func secondsToHoursMinutesSeconds(_ seconds: Int) -> (Int, Int, Int) {
+    var (hrs, mins, secs) = (seconds / 3600, (seconds % 3600) / 60, (seconds % 3600) % 60)
+    hrs = hrs % 24
+    return (hrs, mins, secs)
+}
+
+print(secondsToHoursMinutesSeconds(70 + 70 * 60 + 1 * 60 * 60))
