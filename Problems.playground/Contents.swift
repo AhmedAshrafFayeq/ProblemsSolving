b25d0def8c2ecf1b32e810fad3d8e5760b1789be
Ahmed Fayeq <ahmedashraffayeq@gmail.com>
2022-06-21 23:56:47 +0200

    add solution problem solve

--- Problems.playground/Contents.swift
+++ Problems.playground/Contents.swift
@@ -12,3 +12,20 @@ func secondsToHoursMinutesSeconds(_ seconds: Int) -> (Int, Int, Int) {
 }
 
 print(secondsToHoursMinutesSeconds(70 + 70 * 60 + 1 * 60 * 60))
+
+
+class Solution {
+    func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
+        for i in 0...nums.count-1 {
+            for j in i+1...nums.count-1 {
+                if nums[j] == target-nums[i]{
+                    return [i,j]
+                }
+            }
+        }
+        return [-1,-1]
+    }
+}
+
+var solution = Solution()
+print(solution.twoSum([1,2,3,4], 5))
